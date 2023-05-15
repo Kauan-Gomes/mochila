573564ce5ab9c748fa23eeebcd1324e48992e5f1
Kauan Gomes <Kauang13@gmail.com>
2023-05-15 15:26:23 -0300

    subindo atualizações do codigo

--- js/main.js
+++ js/main.js
@@ -1,47 +1,41 @@
-const form = document.getElementById("novoItem")
+const form = document.getElementById("novoItem") 
 const lista = document.getElementById("lista")
-const itens = JSON.parse(localStorage.getItem("itens")) || []
+const itens = JSON.parse(localStorage.getItem("itens")) || []  
 
-
-itens.forEach((elemento) => {
+itens.forEach( (elemento) => {    
     criaElemento(elemento)
-})
+} )
 
-form.addEventListener("submit", (evento) => {
-    evento.preventDefault() // com o preventDefault estou previnindo o comportamento padrão do forms
-    // evento é o parametro do que esta acontecendo nessa função 
-    //console.log(evento.target[0].value) 
-    //console.log(evento.target.elements['nome'].value) // esse é o valor que eu procuro usando o elements, procurando direto pelo o nome
-    //console.log(evento.target.elements['quantidade'].value)
+form.addEventListener("submit", (evento) => {  
+    evento.preventDefault()
 
     const nome = evento.target.elements['nome']
     const quantidade = evento.target.elements['quantidade']
 
     const itemAtual = {
-        "nome": nome.value,
-        "quantidade": quantidade.value
+    "nome": nome.value,
+    "quantidade": quantidade.value
     }
 
-    criaElemento(itemAtual) // toda vez que esse formulario é submetido eu preciso que essa função seja acionada
+    criaElemento(itemAtual)
 
     itens.push(itemAtual)
 
     localStorage.setItem("itens", JSON.stringify(itens))
 
-    nome.value=""
-    quantidade.value=""
+    nome.value = ""
+    quantidade.value = ""
 })
 
-function criaElemento(item) {
+function criaElemento(item) {  
+    const novoItem = document.createElement('li')
+    novoItem.classList.add("item")
 
-    const novoItem = document.createElement('li') //createelement para criar um novo elemento no documento html
-    novoItem.classList.add("item") //usando classlist para adicioar uma classe
+    const numeroItem = document.createElement('strong')
+    numeroItem.innerHTML = item.quantidade
+    novoItem.appendChild(numeroItem)
 
-    const numeroItem = document.createElement('strong');
-    numeroItem.innerHTML = item.quantidade;
-
-    novoItem.appendChild(numeroItem) // appendChild para colocar um elemento dentro do outro
-    novoItem.innerHTML += item.nome; // incrementando o nome ao novo item 
+    novoItem.innerHTML += item.nome
 
     lista.appendChild(novoItem)
 }
